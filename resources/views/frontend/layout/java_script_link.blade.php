9be4f9a2eb62f0f8db5eafb89a5318ce806d7ac4
HabibPA100 <pahabib50@gmail.com>
2024-12-24 17:57:31 +0600

    Ai massage sender added

--- resources/views/frontend/layout/java_script_link.blade.php
+++ resources/views/frontend/layout/java_script_link.blade.php
@@ -19,3 +19,20 @@
             });
         </script>
 @endif
+
+
+
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/676a96fb49e2fd8dfefcfde0/1ifs5rhq6';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+<!--End of Tawk.to Script-->
